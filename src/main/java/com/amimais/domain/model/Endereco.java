ec4ece52ee9b722ec442844f46dd59959c37cd32
Nesrux <joaomarcosdevs@gmail.com>
2023-10-29 14:03:45 -0300

    Feat: adicionando A Entidade Endereço

---
+++ src/main/java/com/amimais/domain/model/Endereco.java
@@ -0,0 +1,9 @@
+package com.amimais.domain.model;
+
+public class Endereco {
+    private Long id;
+    private Cidade cidade;
+    private String  cep;
+    private String logradouro;
+    private String numero;
+}
