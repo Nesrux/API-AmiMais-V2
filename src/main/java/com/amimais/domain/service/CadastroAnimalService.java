0b9d739c58a22da12a3fdae6d3b333eb885b8c36
Nesrux <joaomarcosdevs@gmail.com>
2023-10-29 15:46:04 -0300

    Feat: adicionando métodos de negocio a classe CadastroAnimalService

--- src/main/java/com/amimais/domain/service/CadastroAnimalService.java
+++ src/main/java/com/amimais/domain/service/CadastroAnimalService.java
@@ -6,18 +6,24 @@ import com.amimais.domain.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CadastroAnimalService {
     @Autowired
     private AnimalRepository animalRepository;
 
-    public Animal cadastrarAnimal(Animal animal){
+    public Animal cadastrarAnimal(Animal animal) {
         return animalRepository.save(animal);
     }
 
-    public Animal acharOuFalhar(Long id){
+    public Animal acharOuFalhar(Long id) {
         return animalRepository.findById(id)
                 .orElseThrow(() -> new AnimalNaoEncontradoException
                         (String.format("O animal de id %d não existe", id)));
+    }
+
+    public List<Animal> buscarTodos() {
+        return animalRepository.findAll();
     }
 }
