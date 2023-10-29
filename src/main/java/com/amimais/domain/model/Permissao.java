c370f8f16e161d415d947794d5dff3091064464e
Nesrux <joaomarcosdevs@gmail.com>
2023-10-29 15:16:28 -0300

    Feat: adicionando a entidade Permissao

---
+++ src/main/java/com/amimais/domain/model/Permissao.java
@@ -0,0 +1,20 @@
+package com.amimais.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Permissao {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descicao;
+}
