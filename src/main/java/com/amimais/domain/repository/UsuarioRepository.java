0ddf41f05b90ef07557794f513789f26446b3950
Nesrux <joaomarcosdevs@gmail.com>
2023-10-29 14:06:41 -0300

    Feat: adicionando o repositório de usuario

---
+++ src/main/java/com/amimais/domain/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.amimais.domain.repository;
+
+import com.amimais.domain.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
