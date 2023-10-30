3f9e09d7e30c010e2fc9f8d89900125fbe721707
Nesrux <joaomarcosdevs@gmail.com>
2023-10-30 14:03:19 -0300

    Feat: Adicionando propriedades para entidade ResgateAnimal e AdocaoAnimal

--- src/main/java/com/amimais/domain/model/animal/AdocaoAnimal.java
+++ src/main/java/com/amimais/domain/model/animal/AdocaoAnimal.java
@@ -1,4 +1,31 @@
 package com.amimais.domain.model.animal;
 
+import com.amimais.domain.model.User.Usuario;
+import com.amimais.domain.model.empresa.Empresa;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class AdocaoAnimal {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Usuario adotante;
+
+    private Empresa ong;
+
+    @Enumerated(EnumType.STRING)
+    private StatusAdocao statusAdocao;
+
+    private OffsetDateTime dataAdocao;
+
+    @UpdateTimestamp
+    private OffsetDateTime ultimaAtualizacao;
 }
--- src/main/java/com/amimais/domain/model/animal/ResgateAnimal.java
+++ src/main/java/com/amimais/domain/model/animal/ResgateAnimal.java
@@ -1,4 +1,32 @@
 package com.amimais.domain.model.animal;
 
+import com.amimais.domain.model.User.Usuario;
+import com.amimais.domain.model.empresa.Empresa;
+import com.amimais.domain.model.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ResgateAnimal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private Empresa ong;
+
+    private Usuario denunciante;
+
+    private Animal animal;
+
+    private String descricao;
+
+    @Embedded
+    private Endereco enderecoDaOcorrencia;
+
+    private StatusResgate status;
 }
